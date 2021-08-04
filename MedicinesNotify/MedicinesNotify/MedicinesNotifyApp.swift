a47fc05285e462fd330d1562e1d6d4665a473e15
d0712936 <d0712936@mail.fcu.edu.tw>
2021-08-04 14:05:52 +0800

    first commit

---
+++ MedicinesNotify/MedicinesNotify/MedicinesNotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedicinesNotifyApp.swift
+//  MedicinesNotify
+//
+//  Created by USER on 2021/8/4.
+//
+
+import SwiftUI
+
+@main
+struct MedicinesNotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
