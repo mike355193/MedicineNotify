0bd3d3ce36d125f3e567896293458e6abe47b577
d0712936 <d0712936@mail.fcu.edu.tw>
2021-08-06 20:02:34 +0800

    - fix delete a medicineItem

--- MedicinesNotify/MedicinesNotify/ContentView.swift
+++ MedicinesNotify/MedicinesNotify/ContentView.swift
@@ -8,9 +8,203 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sel: Int? = nil
+    @State var counters = ["a","b","c"]
+    @State private var action: Int? = nil
+    @State var selection: Int? = nil
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        //Text("Hello, world!")
+        //    .padding()
+        let fakeMedicineItem: MedicineItem = MedicineItem(imgUrl: "photo.fill", name: "medicineName", info:"info", timing: "睡前", timePeriod: MedicineItemTimePeriod(morning: true, noon: true, evening: true), isSelected: false)
+        
+        ////////////////////////////////////////////
+        NavigationView {
+                    VStack {
+                        Text("Main Content View")
+                            .font(.largeTitle)
+                            .fontWeight(.medium)
+                            .foregroundColor(Color.blue)
+                        Spacer()
+                        NavigationLink(destination: AddMedicineFrameView(medicineItem: fakeMedicineItem)) {
+                            Text("Awesome Button")
+                            .frame(minWidth: 0, maxWidth: 300)
+                            .padding()
+                            .foregroundColor(.white)
+                            .background(LinearGradient(gradient: Gradient(colors: [Color.red, Color.blue]), startPoint: .leading, endPoint: .trailing))
+                            .cornerRadius(40)
+                            .font(.title)
+                    }
+                  }
+                    .navigationBarTitle("123")
+                    //.navigationBarHidden(true)
+                    .navigationBarBackButtonHidden(true)
+        }
+        
+        ////////////////////////////////////////////
+        NavigationView {
+                List {
+                    ForEach(counters, id: \.self) { counter in
+                        NavigationLink(destination: ContentView22()
+                        
+                                        
+                        ) {
+                            //CounterCell(counter: counter)
+                            
+                            HStack
+                            {
+                                Image(systemName: "minus.circle.fill")
+                                Text(String(counter))
+                            }
+                            
+                        }
+                    }
+                }
+                .navigationBarTitle("456")
+                .navigationBarHidden(true)
+                .navigationBarBackButtonHidden(true)
+                .navigationBarItems(leading:
+                                        Button(action:
+                                                {
+                                                    
+                                                },
+                                               label:
+                                                {
+                                                    Image(systemName:"chevron.left.circle.fill")
+                                                        .font(.largeTitle)
+                                                }))
+        }
+        
+        ////////////////////////////////////////////
+        NavigationView {
+                //List {
+                VStack{
+                    
+                    //ForEach(counters, id: \.self) { counter in
+                    ForEach(0..<counters.count) { counter in
+                        
+                        HStack
+                        {
+                            NavigationLink(destination: Text(counters[counter]), tag: counter, selection: $sel) {
+                                EmptyView()
+                            }
+                            Image(systemName: "minus.circle.fill")
+                                .onTapGesture {
+                                    //perform some tasks if needed before opening Destination view
+                                    self.sel = counter
+                                }
+                                .frame(width: 20, height:20)
+                            
+                            
+                            Text("123")
+                            //Text(counters[counter])
+                            Text(String(counter))
+                            //Text(counter)
+                            Spacer()
+                            Text("789")
+                        }
+                        
+                        
+                    }
+                    
+                //}
+                }
+        }
+
+        ////////////////////////////////////////////
+        NavigationView {
+            List
+            {
+                HStack
+                {
+                    Text("1111")
+                    
+                    NavigationLink(destination: Text("Test"), tag: 1, selection: $selection) {
+                        
+                        Button(action: {
+                            print("login tapped")
+                            self.selection = 1
+                        }) {
+                            HStack {
+                                Spacer()
+                                Text("Login").foregroundColor(Color.white).bold()
+                                Spacer()
+                            }
+                            
+                        }
+                        .accentColor(Color.black)
+                        .padding()
+                        .background(Color(UIColor.darkGray))
+                        .cornerRadius(4.0)
+                        .padding(Edge.Set.vertical, 20)
+                    }
+                }
+                
+                HStack
+                {
+                    Text("2222")
+                    
+                    NavigationLink(destination: Text("Test222"), tag: 2, selection: $selection) {
+                        
+                        
+                        Button(action: {
+                            print("login tapped")
+                            self.selection = 2
+                        }) {
+                            HStack {
+                                Spacer()
+                                Text("Login").foregroundColor(Color.white).bold()
+                                Spacer()
+                            }
+                            
+                        }
+                        .accentColor(Color.black)
+                        .padding()
+                        .background(Color(UIColor.darkGray))
+                        .cornerRadius(4.0)
+                        .padding(Edge.Set.vertical, 20)
+                    }
+                }
+            }
+        }
+        ////////////////////////////////////////////
+    }
+}
+    
+    struct CounterCell: View {
+
+    @State var counter: String
+    @State var inc = 0
+
+    var body: some View {
+        HStack {
+            Button(action: { self.inc += 1 }) {
+                Text("plus")
+            }
+            Button(action: { self.inc -= 1 }) {
+                Text("minus")
+            }
+            Text(" counter: \(counter) value: \(inc)")
+        }
+    }
+    }
+
+struct ContentView22: View {
+    
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body: some View {
+        Text("123123")
+        .navigationBarBackButtonHidden(true)
+        .navigationBarItems(leading:
+            Button(action: {
+                self.presentationMode.wrappedValue.dismiss()
+            }, label: {
+                Image(systemName: "chevron.left.circle.fill")
+                    .font(.largeTitle)
+                    .foregroundColor(.black)
+            })
+        )
     }
 }
 
--- MedicinesNotify/MedicinesNotify/MyMedicineList/EditMyListView.swift
+++ MedicinesNotify/MedicinesNotify/MyMedicineList/EditMyListView.swift
@@ -60,101 +60,107 @@ struct EditMyListView: View {
 }
 
 struct MedicineItemsView: View {
+    @State var listItems = ["Item 1", "Item 2", "Item 3"]
+    @State var medicineItems: MedicineItems = JsonParserInRoot("MedicineItems.json")
     var body: some View {
         // load file MedicineItems.json to get all items
-        var medicineItems: MedicineItems = JsonParserInRoot("MedicineItems.json")
+        //var medicineItems: MedicineItems = JsonParserInRoot("MedicineItems.json")
         VStack
         {
-            List()
+            NavigationView
             {
-                // defaults
-                ForEach(0..<medicineItems.defaults.count)
+                List()
                 {
-                    index in
-                    MedicineItemView(medicineItem: medicineItems.defaults[index])
+                    ForEach(0..<medicineItems.defaults.count)
+                    {
+                        index in
+                        if (index < medicineItems.defaults.count)
+                        {
+                            MedicineItemView(medicineItem: medicineItems.defaults[index])
+                        }
+                    }
+                    // customs
+                    ForEach(0..<self.medicineItems.custom.count)
+                    {
+                        index in
+                        if (index < medicineItems.custom.count)
+                        {
+                            MedicineItemView(medicineItem: self.medicineItems.custom[index])
+                        }
+                    }
+                    .onDelete
+                    {
+                        (indexSet) in
+                        self.medicineItems.custom.remove(atOffsets: indexSet)
+                    }
                 }
-                .onDelete
-                {
-                    (index) in
-                    medicineItems.defaults.remove(atOffsets: index)
-                }
-                // customs
-                ForEach(0..<medicineItems.custom.count)
-                {
-                    index in
-                    MedicineItemView(medicineItem: medicineItems.custom[index])
-                }
-                .onDelete
-                {
-                    (index) in
-                    medicineItems.custom.remove(atOffsets: index)
-                }
+                .navigationBarTitle("回到我的藥單")
+                .navigationBarHidden(true)
             }
         }
     }
+}
+
+struct Safe<T: RandomAccessCollection & MutableCollection, C: View>: View {
+   
+   typealias BoundElement = Binding<T.Element>
+   private let binding: BoundElement
+   private let content: (BoundElement) -> C
+
+   init(_ binding: Binding<T>, index: T.Index, @ViewBuilder content: @escaping (BoundElement) -> C) {
+      self.content = content
+      self.binding = .init(get: { binding.wrappedValue[index] },
+                           set: { binding.wrappedValue[index] = $0 })
+   }
+   
+   var body: some View {
+      content(binding)
+   }
 }
 
 struct MedicineItemView: View {
     let medicineItem: MedicineItem
     
     var body: some View {
-        HStack
+        ZStack
         {
-            Group
+            // view
+            HStack
             {
                 // according state to do that to show
                 // state = selected
                 if(medicineItem.isSelected == true)
                 {
-                    //NavigationLink(destination: AddMedicineFrameView(medicineItem: medicineItem))
-                    NavigationLink(destination: ContentView())
-                    {
-                        Image(systemName: "minus.circle.fill")
-                    }
-                    //Image(systemName: "minus.circle.fill")
+                    Image(systemName: "minus.circle.fill")
                 }
                 // state = not selected
                 else
                 {
-                    NavigationLink(destination: AddMedicineFrameView(medicineItem: medicineItem))
-                    {
-                        Image(systemName: "plus.circle.fill")
-                    }
+                    Image(systemName: "plus.circle.fill")
+                }
+                
+                Text(medicineItem.name)
+                Spacer()
+                Image(systemName: medicineItem.imgUrl)
+            }
+            
+            // event
+            if(medicineItem.isSelected == true)
+            {
+                // do some thing remove things. after set, update file into MedicinesItem.json
+                
+            }
+            // state = not selected
+            else
+            {
+                // nav to AddMedicineFrameView. after set, update file into MedicinesItem.json
+                NavigationLink(destination: AddMedicineFrameView(medicineItem: medicineItem))
+                {
+                    EmptyView()
                 }
             }
-            .frame(width: 20, height:20)
-            //.hidden()
-            
-            
-            /*
-            Button(action:
-                    {
-                        // according state to do that to do
-                        // state = selected
-                        
-                        // state = not selected
-                        //AddMedicineFrameView(medicineItem: medicineItem)
-                    },
-                   label:
-                    {
-                        // according state to do that to show
-                        // state = selected
-                        if(medicineItem.isSelected == true)
-                        {
-                            Image(systemName: "minus.circle.fill")
-                        }
-                        // state = not selected
-                        else
-                        {
-                            Image(systemName: "plus.circle.fill")
-                        }
-                    })
-            */
-            
-            Text(medicineItem.name)
-            Spacer()
-            Image(systemName: medicineItem.imgUrl)
         }
+        
     }
 }
 
